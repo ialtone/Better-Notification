168135d83fb21b500ed71f8ae168e5a629b30d68
ialtone <1425592567@qq.com>
2024-07-23 23:36:16 +0800

    修改

---
+++ src/pages/style2/syle2.vue
@@ -0,0 +1,45 @@
+<script setup>
+</script>
+
+<template>
+    <div class="box"></div>
+</template>
+
+
+<style scoped>
+.box {
+    position: relative;
+    width: 200px;
+    height: 200px;
+    background-color: #fff;
+    border-radius: 10px;
+}
+
+.box::before,
+.box::after {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    border: 2px solid #000;
+    border-radius: inherit;
+}
+
+.box::before {
+    top: 5px;
+    left: 5px;
+    right: 5px;
+    bottom: 5px;
+    border-width: 1px;
+}
+
+.box::after {
+    top: 10px;
+    left: 10px;
+    right: 10px;
+    bottom: 10px;
+    border-width: 2px;
+}
+</style>
--- src/router/router.ts
+++ src/router/router.ts
@@ -2,7 +2,13 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 const routes = [
     {
         path: '/',
+        name: 'style1',
         component: () => import('../pages/style1/style1.vue'),
+    },
+    {
+        path: '/style2',
+        name: 'style2',
+        component: () => import('../pages/style2/style2.vue'),
     }
 ];
 
